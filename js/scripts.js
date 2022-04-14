249ab02c8c2a16d3a9b185c0ddf9b11ab66c78c8
Daniel Alexander Muelas Rivera <danielmuelasrivera483@gmail.com>
2022-04-14 15:10:09 -0500

    Finalizada la primera parte de la landing-page en proceso de edicion de los videos

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', event => {
 
     // Navbar shrink function
-    var navbarShrink = function () {
+    let navbarShrink = function () {
         const navbarCollapsible = document.body.querySelector('#mainNav');
         if (!navbarCollapsible) {
             return;
@@ -43,3 +43,54 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+//portafolio
+filterSelection("mobile");
+
+function filterSelection(filter) {
+  let columnGallery, i;
+  columnGallery = document.getElementsByClassName("column-gallery");
+  if (filter == "all") filter = "";
+  for (i = 0; i < columnGallery.length; i++) {
+    w3RemoveClass(columnGallery[i], "show");
+    if (columnGallery[i].className.indexOf(filter) > -1) w3AddClass(columnGallery[i], "show");
+  }
+}
+
+function w3AddClass(element, name) {
+  let i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
+
+function w3RemoveClass(element, name) {
+  let i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+
+// Add active class to the current button (highlight it)
+const btnContainer = document.getElementById("myBtnContainer");
+const btns = btnContainer.getElementsByClassName("btn-gallery");
+
+for (let i = 0; i < btns.length; i++) {
+
+  btns[i].addEventListener("click", function(){
+    
+    let current = document.getElementsByClassName("active-gallery");
+    current[0].className = current[0].className.replace(" active-gallery", "");
+    this.className += " active-gallery";
+    
+  });
+
+}
